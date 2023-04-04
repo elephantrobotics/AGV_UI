e5708d0672eadbe84728653087f8783e2d52f09a
wangWking <842749351@qq.com>
2023-04-04 19:07:05 +0800

    update and add server.py

--- main.py
+++ main.py
@@ -23,8 +23,6 @@ from pymycobot.mycobotsocket import MyCobotSocket
 from libraries.pyqtfile.agv_UI import Ui_AGV_UI as AGV_Window
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
-from scripts.end import *
-from scripts import test_auto_charing
 
 
 class AGV_APP(AGV_Window, QMainWindow, QWidget):
@@ -57,64 +55,15 @@ class AGV_APP(AGV_Window, QMainWindow, QWidget):
         self.mycobot_ip = '192.168.123.23'
         self.mycobot_port = 9000
         self.mc = MyCobotSocket(self.mycobot_ip, self.mycobot_port)
+        
+        self.agv_ip = ''
+        self.agv_port = 9002
 
         # 上料区是否有新木块放置
         self.feed_place = True
         # 上料区吸取次数
         self.pump_times = 0
 
-        import RPi.GPIO as GPIO
-        GPIO.setwarnings(False)
-        self.GPIO = GPIO
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(20, GPIO.OUT)
-        GPIO.setup(21, GPIO.OUT)
-        GPIO.output(20, 1)
-        GPIO.output(21, 1)
-
-        # 移动角度
-        self.move_angles = [
-            [0, 0, 0, 0, 80, 0],  # init the point
-            [-40.31, 2.02, -10.72, -0.08, 80, -54.84],  # point to grab
-            [-70.31, 2.02, -10.72, -0.08, 80, -54.84]
-        ]
-
-        # 移动坐标
-        self.move_coords = [
-            [96.5, -101.9, 185.6, 155.25, 19.14, 75.88],  # above the red bucket
-            [180.9, -99.3, 184.6, 124.4, 30.9, 80.58],  # above the green bucket
-            [77.4, 122.1, 179.2, 151.66, 17.94, 178.24],  # above the blue bucket
-            [2.2, 128.5, 171.6, 163.27, 10.58, -147.25]  # gray
-        ]
-
-        # agv 车上角度
-        self.avg_angles = [
-            [-2.54, 14.67, -177.36, 0.87, -88.24, -7.73],
-            [-159, 20.3, -16.69, 1.84, 77.34, -54.75],
-            [-2.72, -4.74, -168.66, 1.14, -96.76, -47.63],
-            [-25.13, -5.53, -176.04, 1.23, -84.81, -38.4],
-            [12.12, -23.64, -153.8, 1.49, -84.63, -36.12],
-            [-3.77, -25.75, -153.1, 1.23, -84.9, -36.38],
-            [-19.59, -27.07, -147.12, 2.46, -88.68, -36.38]
-        ]
-
-        # place angles
-        self.agv_place_angles = [
-            [-2.54, 14.67, -177.36, 0.87, -88.24, -7.73],
-            [19.07, -16.52, -176.3, 0.43, -79.54, -39.9],
-            [-27.94, -18.72, 179.64, 5.36, -70.4, -46.93],
-            [-22.67, -23.29, -172.35, 1.14, -83.4, -39.72],
-            [12.3, -39.19, -147.65, 2.19, -79.89, -39.72],
-            [-5.27, -40.69, -146.6, 2.72, -82.0, -39.99],
-            [-17.75, -41.74, -146.86, 0.87, -79.45, -39.9]
-        ]
-
-        # down_angles
-        self.down_place_angles = [
-            [29.44, -19.16, 21.35, 0.61, 33.48, -76.55],
-            [-4.74, -20.56, 21.35, 0.26, 43.24, -76.72],
-            [-43.33, 13.27, -20.83, 3.51, 53.7, -76.72]
-        ]
         # 创建一个服务器套接字
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -127,185 +76,33 @@ class AGV_APP(AGV_Window, QMainWindow, QWidget):
         self.t_agv = threading.Thread(target=self.myagv_loop_run)
         self.t_agv.daemon = True
         self.t_agv.start()
+            
+    def socket_connect(self, x, y=0):
+        import socket
 
-    def myagv_loop_run(self):
-        try:
-            for i in range(3):
-                self.down_position()
-                self.feed_position()
-        except Exception as e:
-            print(e)
+        # 创建一个客户端套接字
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        # 连接服务器
+        server_address = (self.agv_ip, self.agv_port)
+        client_socket.connect(server_address)
+
+        data = struct.pack('ff', x, y)
+
+        # 向服务器发送数据
+        client_socket.sendall(data)
+
+        # 接收来自服务器的响应
+        response = client_socket.recv(1024)
+        print(f"Received response from server: {response.decode()}")
+
+        # 关闭客户端套接字
+        client_socket.close()
     def moved(self, x, y):
         print('x, y', x, y)
-        # send Angle to move mecharm 270
-        self.mc.send_angles(self.move_angles[0], 50)
-        time.sleep(2)
+        
+        self.socket_connect(x, y)
 
-        # [x, y, 185.5, -157.73, 22.08, -140.19]
-        # send coordinates to move mycobot
-        if x <= 180:
-            print(1)
-            self.mc.send_coords([x, y, 150, -176.1, 2.4, -125.1], 30, 0)  # usb :rx,ry,rz -173.3, -5.48, -57.9
-            time.sleep(4)
-
-            # self.mc.send_coords([x, y, 150, 179.87, -3.78, -62.75], 25, 0)
-            # time.sleep(3)
-
-            # self.mc.send_coords([x, y, 105, 179.87, -3.78, -62.75], 25, 0)
-            self.mc.send_coords([x, y, 140, -176.1, 2.4, -125.1], 30, 0)
-        else:
-            if x > 188:
-                x = 174
-            else:
-                x = x - 20
-            print(2)
-            self.mc.send_coords([x, y, 175, -164.99, 11.33, -125.91], 30,
-                                0)  # [174.6, -3.5, 175.8, -164.99, 11.33, -125.91]
-            time.sleep(4)
-
-            # self.mc.send_coords([x, y, 150, 179.87, -3.78, -62.75], 25, 0)
-            # time.sleep(3)
-
-            # self.mc.send_coords([x, y, 105, 179.87, -3.78, -62.75], 25, 0)
-            self.mc.send_coords([x, y, 148, -164.99, 11.33, -125.91], 30, 0)
-        time.sleep(4)
-
-        # open pump
-        self.gpio_status(True)
-        time.sleep(1.5)
-        if self.pump_times < 4:
-            self.pump_times += 1
-        else:
-            self.feed_place = False
-            self.pump_times = 1
-
-        tmp = []
-        while True:
-            if not tmp:
-                tmp = self.mc.get_angles()
-            else:
-                break
-        time.sleep(0.5)
-
-        print('tmp:', tmp)
-        self.mc.send_angles([tmp[0], 17.22, -32.51, tmp[3], 80, tmp[5]],
-                            40)  # [18.8, -7.91, -54.49, -23.02, -0.79, -14.76]
-        time.sleep(1)
-        self.mc.send_angles([-70, 17.22, -32.51, tmp[3], 80, tmp[5]],
-                            40)  # [18.8, -7.91, -54.49, -23.02, -0.79, -14.76]
-        time.sleep(2.5)
-        self.mc.send_angles([-70, 14.67, -177.36, 0.87, -88.24, -7.73], 40)
-        time.sleep(3.5)
-        self.mc.send_angles(self.avg_angles[0], 40)
-        time.sleep(2.5)
-        # temp_ang=self.avg_angles[self.pump_times][1]
-        # self.avg_angles[self.pump_times][1] = -10
-        # self.mc.send_angles(self.avg_angles[self.pump_times], 40)
-        # time.sleep(2.5)
-        # self.avg_angles[self.pump_times][1] = temp_ang
-        print(self.pump_times)
-        print(self.avg_angles[self.pump_times])
-        if self.pump_times > 3:
-            self.mc.send_angles([-4.92, -10.81, -151.43, 2.54, -80.15, -10.28], 40)
-            time.sleep(1.5)
-        self.mc.send_angles(self.avg_angles[self.pump_times], 40)
-        time.sleep(3)
-
-        # close pump
-        self.gpio_status(False)
-        time.sleep(3)
-        self.mc.send_angles([-70, 14.67, -177.36, 0.87, -88.24, -7.73], 40)
-        time.sleep(1.5)
-
-        self.mc.send_angles([-70, 17.22, -32.51, tmp[3], 97, tmp[5]],
-                            40)  # [18.8, -7.91, -54.49, -23.02, -0.79, -14.76]
-        time.sleep(3.5)
-        self.mc.send_angles(self.move_angles[1], 50)
-        time.sleep(3)
-
-    def place(self):
-
-        self.pump_times = 4
-        for i in range(1, self.pump_times):
-            self.mc.send_angles(self.move_angles[1], 40)
-            time.sleep(3)
-            self.mc.send_angles(self.move_angles[2], 40)
-            time.sleep(1)
-            self.mc.send_angles([-70, 14.67, -177.36, 0.87, -88.24, -7.73], 40)
-            time.sleep(3.5)
-            self.mc.send_angles([self.avg_angles[i][0], 14.67, -177.36, 0.87, -88.24, -7.73], 40)
-            time.sleep(1.5)
-            if i > 3:
-                self.mc.send_angles([self.avg_angles[i][0], -10.81, -151.43, 2.54, -80.15, -10.28], 40)
-                time.sleep(1.5)
-            self.mc.send_angles(self.agv_place_angles[i], 40)
-            time.sleep(3)
-            # open pump
-            self.gpio_status(True)
-            time.sleep(2.5)
-            ang = self.agv_place_angles[i][1] + 30
-            self.mc.send_angle(2, ang, 40)
-            time.sleep(1)
-            self.mc.send_angles([-70, 14.67, -177.36, 0.87, -88.24, -7.73], 40)
-            time.sleep(1.5)
-            self.mc.send_angles(self.move_angles[2], 40)
-            time.sleep(3.5)
-            ang = self.move_angles[1][2] - 30
-            self.mc.send_angle(2, ang, 40)
-            time.sleep(1.5)
-            if i == 0:
-                self.mc.send_angles(self.down_place_angles[0], 40)
-            elif i == 1:
-                self.mc.send_angles(self.down_place_angles[1], 40)
-            else:
-                self.mc.send_angles(self.down_place_angles[2], 40)
-            time.sleep(3)
-            # close pump
-            self.gpio_status(False)
-
-    def gpio_status(self, flag):
-        if flag:
-            GPIO.output(20, 0)
-            GPIO.output(21, 0)
-        else:
-            # GPIO.output(20, 1)
-            GPIO.output(21, 1)
-
-    def responsed(self):
-        t = time.time()
-        while True:
-            x = None
-            y = None
-            # 等待客户端连接
-            client_socket, client_address = self.server_socket.accept()
-            print(f"Connection from {client_address} has been established.")
-
-            # 接收来自客户端的数据
-            data = client_socket.recv(1024)
-            x, y = struct.unpack('ff', data)
-            # print(f"Received data from client: {data.decode()}")
-            print(f"Received data from client: {x},{y}")
-            # 向客户端发送响应
-            response = "Data received successfully!"
-            client_socket.sendall(response.encode())
-
-            if x and y:
-                t = time.time()
-                self.feed_place = True
-                self.moved(x, y)
-                if not self.feed_place:
-                    break
-
-            else:
-                if time.time() - t > 10:
-                    self.feed_place = False
-                    break
-
-            response = "Running END!"
-            client_socket.sendall(response.encode())
-            # 关闭客户端套接字
-            client_socket.close()
 
     def btn_color(self, btn, color):
         if color == 'red':
@@ -338,6 +135,7 @@ class AGV_APP(AGV_Window, QMainWindow, QWidget):
         self.start_btn.setEnabled(False)
         self.btn_color(self.puase_btn, 'blue')
         self.puase_btn.setEnabled(True)
+        self.socket_connect('start')
 
     def pause_run(self):
         """
@@ -350,6 +148,7 @@ class AGV_APP(AGV_Window, QMainWindow, QWidget):
         self.start_btn.setEnabled(True)
         self.btn_color(self.puase_btn, 'red')
         self.puase_btn.setEnabled(False)
+        self.socket_connect('pause')
 
     def down_position(self):
         """
@@ -357,26 +156,28 @@ class AGV_APP(AGV_Window, QMainWindow, QWidget):
         :return:
         """
         print('down-position-btn')
-        try:
-            set_robot_pose = SetRobotPose()
-            set_robot_pose.set_pose()
-            map_navigation = MapNavigation1()
-            map_navigation.navigate()
-            map_navigation = MapNavigation2()
-            map_navigation.navigate()
-            test_auto_charing.init()
-            while self.pause_clicked:
-                self.stop_wait(0.2)
+        self.socket_connect('down')
+        # import socket
 
-            test_auto_charing.main()
-            self.place()
-            while self.pause_clicked:
-                self.stop_wait(0.2)
-        except Exception as e:
-            print(e)
-            e = traceback.format_exc()
-            with open('./error.log', 'a+') as f:
-                f.write(e)
+        # # 创建一个客户端套接字
+        # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # # 连接服务器
+        # server_address = (self.agv_ip, self.agv_port)
+        # client_socket.connect(server_address)
+        # x = 0
+        # y = 0
+        # data = struct.pack('ff', x, y)
+
+        # # 向服务器发送数据
+        # client_socket.sendall(data)
+
+        # # 接收来自服务器的响应
+        # response = client_socket.recv(1024)
+        # print(f"Received response from server: {response.decode()}")
+
+        # # 关闭客户端套接字
+        # client_socket.close()
 
     def feed_position(self):
         """
@@ -384,27 +185,29 @@ class AGV_APP(AGV_Window, QMainWindow, QWidget):
         :return:
         """
         print('feed-position-btn')
-        try:
-            map_navigation = MapNavigation3()
-            map_navigation.navigate()
-            map_navigation = MapNavigation4()
-            map_navigation.navigate()
-            test_auto_charing.init()
-            while self.pause_clicked:
-                self.stop_wait(0.2)
-            test_auto_charing.main()
-            t = time.time()
-            while self.feed_place:
-                self.responsed()
-            while self.pause_clicked:
-                self.stop_wait(0.2)
-            set_robot_pose = SetRobotPose()
-            set_robot_pose.set_pose()
-        except Exception as e:
-            print(e)
-            e = traceback.format_exc()
-            with open('./error.log', 'a+') as f:
-                f.write(e)
+        self.socket_connect('feed')
+        # import socket
+
+        # # 创建一个客户端套接字
+        # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # # 连接服务器
+        # server_address = (self.agv_ip, self.agv_port)
+        # client_socket.connect(server_address)
+        # x = -1
+        # y = -1
+        # data = struct.pack('ff', x, y)
+
+        # # 向服务器发送数据
+        # client_socket.sendall(data)
+
+        # # 接收来自服务器的响应
+        # response = client_socket.recv(1024)
+        # print(f"Received response from server: {response.decode()}")
+
+        # # 关闭客户端套接字
+        # client_socket.close()
+        
 
     # Initialize variables
     def _init_variable(self):
